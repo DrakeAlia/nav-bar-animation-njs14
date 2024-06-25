56a12d4faf5b62f5fe794b4c1ac63f14cb95d441
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2024-06-25 15:45:56 -0700

    first commit

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>You can reach me at:</p>
+      <ul>
+        <li>
+          Email: <a href="mailto: [email protected]"> [email protected]</a>
+        </li>
+        <li>
+          Twitter: <a href="https://twitter.com/username">username</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
